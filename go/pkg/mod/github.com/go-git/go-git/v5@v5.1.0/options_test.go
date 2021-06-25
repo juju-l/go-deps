e367c21abb5b63add3efd69e70af89dc34f2f2ff
root <lj@vipex.cc>
2021-06-25 11:00:27 +0800

    go mod tidy

---
+++ go/pkg/mod/github.com/go-git/go-git/v5@v5.1.0/options_test.go
@@ -0,0 +1,29 @@
+package git
+
+import (
+	"github.com/go-git/go-git/v5/plumbing/object"
+	. "gopkg.in/check.v1"
+)
+
+type OptionsSuite struct {
+	BaseSuite
+}
+
+var _ = Suite(&OptionsSuite{})
+
+func (s *OptionsSuite) TestCommitOptionsParentsFromHEAD(c *C) {
+	o := CommitOptions{Author: &object.Signature{}}
+	err := o.Validate(s.Repository)
+	c.Assert(err, IsNil)
+	c.Assert(o.Parents, HasLen, 1)
+}
+
+func (s *OptionsSuite) TestCommitOptionsCommitter(c *C) {
+	sig := &object.Signature{}
+
+	o := CommitOptions{Author: sig}
+	err := o.Validate(s.Repository)
+	c.Assert(err, IsNil)
+
+	c.Assert(o.Committer, Equals, o.Author)
+}
